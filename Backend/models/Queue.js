2ab616528ec8286e41788557cd61215942def188
ParthSaini980 <parth234saini@gmail.com>
2026-08-11 21:25:36 +0530

    Add Queue model with schema and timestamps

--- Backend/models/Queue.js
+++ Backend/models/Queue.js
@@ -1,1 +1,37 @@
+import mongoose from "mongoose";
 
+const queueSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+
+    createdBy: {
+  type: mongoose.Schema.Types.ObjectId,
+  ref: "User",
+  required: true,
+},
+
+    users: [
+  {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: false,
+  },
+],
+
+    status: {
+      type: String,
+      enum: ["active", "closed"],
+      default: "active",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Queue = mongoose.model("Queue", queueSchema);
+
+export default Queue;
