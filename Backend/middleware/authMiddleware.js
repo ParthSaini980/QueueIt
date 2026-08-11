b0c45ccf7cc34fca3a6bc3e8bfd78e6f17e50904
ParthSaini980 <parth234saini@gmail.com>
2026-08-11 22:10:51 +0530

    Update authMiddleware.js

--- Backend/middleware/authMiddleware.js
+++ Backend/middleware/authMiddleware.js
@@ -1,5 +1,7 @@
 import jwt from "jsonwebtoken";
 
+// QueueIt authentication middleware
+
 const authMiddleware = (req, res, next) => {
   try {
     const token = req.headers.authorization?.split(" ")[1];
